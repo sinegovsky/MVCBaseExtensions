951fdeb7ffb57b4a63f901580fe5723683862cea
Sinegovsky Ivan <sivan.svm@gmail.com>
2013-05-28 14:03:26 +0700

    Some settings

---
+++ MvcBaseExtensions/Base/BaseController.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BaseController.cs" company="Sinegovsky Ivan">
+//   Sinegovsky Ivan, LLC 2013
+// </copyright>
+// <summary>
+//   Base MVC controlelr
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace MvcBaseExtensions.Base
+{
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// Base MVC controlelr
+    /// </summary>
+    public abstract class BaseController : Controller
+    {
+        /// <summary>
+        /// Return JSON success result
+        /// </summary>
+        /// <param name="data">data to return</param>
+        /// <returns>JSON object</returns>
+        public JsonResult Success(object data)
+        {
+            return this.Json(new { success = true, response = data });
+        }
+
+        /// <summary>
+        /// Return JSON error result
+        /// </summary>
+        /// <param name="message">error message</param>
+        /// <returns>JSON object</returns>
+        public JsonResult Error(string message)
+        {
+            return this.Json(new { success = false, response = message });
+        }
+    }
+}
---
+++ MvcBaseExtensions/Scripts/Core.js
@@ -0,0 +1,47 @@
+﻿var Core = {};
+
+/// <summary>Load JSON by AJAX/</summary>
+/// <param name="url" type="String">URL for load.</param>
+/// <param name="successCallback" type="Function">Success function (optional).</param>
+/// <param name="data" type="Object">POST data (optional).</param>
+/// <param name="errorCallback" type="Function">Error function (optional).</param>
+Core.ajax = function(url, successCallback, data, errorCallback) {
+    jQuery.ajax({
+            cache: false,
+            dataType: 'json',
+            data: data,
+            type: 'POST',
+            url: url,
+            success: function(resp) {
+                if (resp.success) {
+                    if (!successCallback)
+                        return;
+                    successCallback();
+                } else {
+                    if (!errorCallback) {
+                        Core.error(resp.response);
+                    } else {
+                        errorCallback();
+                    }
+                }
+            },
+            error: function(resp) {
+                Core.error(resp);
+            }
+        }
+    );
+};
+
+Core.loadFunctions = [];
+
+/// <summary>Exec function on document load/</summary>
+/// <param name="func" type="Function">Function for load.</param>
+Core.load = function(func) {
+    Core.loadFunctions.push(func);
+};
+
+/// <summary>Display error message/</summary>
+/// <param name="message" type="String">Message for display.</param>
+Core.error = function(message) {
+    alert('ERROR: ' + message);
+};
